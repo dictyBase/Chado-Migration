01fe15f9b2f5997d4a5207fae4abd46e84b00482
Siddhartha Basu <biosidd@gmail.com>
2011-10-22 02:25:33 -0500

    added version file

---
+++ db/OraclePlus/deploy/0.058/001-auto-__VERSION.sql
@@ -0,0 +1,31 @@
+-- 
+-- Created by SQL::Translator::Producer::Oracle
+-- Created on Sat Oct 22 02:19:22 2011
+-- 
+--
+-- Table: chadoprop
+--;
+
+CREATE SEQUENCE "sq_chadoprop_chadoprop_id";
+
+CREATE TABLE "chadoprop" (
+  "chadoprop_id" number NOT NULL,
+  "type_id" number NOT NULL,
+  "value" clob,
+  "rank" number DEFAULT '0' NOT NULL,
+  PRIMARY KEY ("chadoprop_id"),
+  CONSTRAINT "chadoprop_c1" UNIQUE ("type_id", "rank")
+);
+
+CREATE OR REPLACE TRIGGER "ai_chadoprop_chadoprop_id"
+BEFORE INSERT ON "chadoprop"
+FOR EACH ROW WHEN (
+ new."chadoprop_id" IS NULL OR new."chadoprop_id" = 0
+)
+BEGIN
+ SELECT "sq_chadoprop_chadoprop_id".nextval
+ INTO :new."chadoprop_id"
+ FROM dual;
+END;
+/
+
