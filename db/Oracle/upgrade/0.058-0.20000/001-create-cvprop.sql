e5c636f0fc8457ef7bb1bbc926ae59512a318a99
Siddhartha Basu <biosidd@gmail.com>
2013-01-02 16:52:49 -0600

    sql statements for adding cvprop table perl script to disable all audit triggers

---
+++ db/Oracle/upgrade/0.058-0.20000/001-create-cvprop.sql
@@ -0,0 +1,34 @@
+
+BEGIN;
+
+CREATE SEQUENCE sq_cvprop_cvprop_id;
+
+CREATE TABLE cvprop (
+  cvprop_id number(11) NOT NULL,
+  cv_id number(10) NOT NULL,
+  type_id number(10) NOT NULL,
+  value clob,
+  rank number(10) DEFAULT '0' NOT NULL,
+  PRIMARY KEY (cvprop_id),
+  CONSTRAINT cvprop_c1 UNIQUE (cv_id, type_id, rank)
+);
+
+
+ALTER TABLE cvprop ADD CONSTRAINT cvprop_cv_id_fk FOREIGN KEY (cv_id) REFERENCES cv (cv_id);
+
+ALTER TABLE cvprop ADD CONSTRAINT cvprop_type_id_fk FOREIGN KEY (type_id) REFERENCES cvterm (cvterm_id);
+
+
+CREATE OR REPLACE TRIGGER ai_cvprop_cvprop_id
+BEFORE INSERT ON cvprop
+FOR EACH ROW WHEN (
+ new.cvprop_id IS NULL OR new.cvprop_id = 0
+)
+BEGIN
+ SELECT sq_cvprop_cvprop_id.nextval
+ INTO :new.cvprop_id
+ FROM dual;
+END;
+
+
+COMMIT;
