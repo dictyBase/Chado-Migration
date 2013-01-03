586dba8143ff7ccae934e7f5d6ffafcb3668754e
Siddhartha Basu <biosidd@gmail.com>
2013-01-03 06:37:18 -0600

    use v4 naming for schema,  otherwise it throws a big warning

--- db/Oracle/upgrade/0.058-0.20000/001-disable-audit-triggers.pl
+++ db/Oracle/upgrade/0.058-0.20000/001-disable-audit-triggers.pl
@@ -4,7 +4,7 @@ use warnings;
 use DBIx::Class::DeploymentHandler::DeployMethod::SQL::Translator::ScriptHelpers
    'schema_from_schema_loader';
     
-schema_from_schema_loader({ naming => 'current' },  sub {
+schema_from_schema_loader({ naming => 'v4' },  sub {
 	my ($schema, $version_set) = @_;
 
         $schema->storage->dbh_do(
